da45faf5646003fe206c470c731c2ba8675ccb9c
tofaareta123 <47651534+tofaareta123@users.noreply.github.com>
2019-11-18 18:10:00 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,6 @@
+<?
+/*gati dengan auto refres kalian*/
+
+$refres = "{"refresh_token":"AEu4IL1r0LMvF_mEgv2pGwO24TlxYLGQZxlLvwD3qlByraS5Mo1aFKKGWtbiW6huVrqHsCZVE_CN0qAHdsDPDHWLNn9u8rP9DBzfzXhYL9Yif8rvro75US1RiYyLia6rsVhbAWHaqLQVsAjW4ULJaRxTzHB_YfuBY0PZrBsWfLPOawW3RRn_YYjp5TDRQhzqLm61O8e_SptMJZ0dslVk2s1l9rJIk-He5GZn1J_EBi-bnpSGwEsHJRQQPx8to5sAOFJaepFkfMf-iZfHwSlvzVcK57HH8vW-N8RdPq-_vEzsPQkWaHHfLKLIyJGFFU40ukEx9PJ9dI99uZ0doBDDAfBb2RvjjgdeCU3OjgsnGQbHNq9mh3vokVxaRsHgijKuGjMo028AEQS7ydvDvpMJY-WCQjIlrcuRdbL8V5r94azurVws-xI6guo";
+/*link pas https://semawur.com/vn3ER2a       */
+$pass_scrip = "pongpong";
